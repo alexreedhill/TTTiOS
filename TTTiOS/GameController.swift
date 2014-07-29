3c72c861b8c76635be0d57fc1af5813e46bb6c79
alexreedhill <ahill1994@student.elgin.edu>
2014-07-29 15:04:21 -0500

    menu controller gets game options
    transition to game controller
    game controller uses web view to call local ttt-rb

---
+++ TTTiOS/GameController.swift
@@ -0,0 +1,34 @@
+//
+//  GameController.swift
+//  TTTiOSWebView
+//
+//  Created by Alex Hill on 7/29/14.
+//  Copyright (c) 2014 Alex Hill. All rights reserved.
+//
+
+import UIKit
+
+class GameController: UIViewController {
+    @IBOutlet strong var web1: UIWebView?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        var url = NSURL(string: "http://localhost:9393")
+        var request = NSURLRequest(URL: url)
+        
+        if let webView = web1 {
+            webView.loadRequest(request)
+        } else {
+            println("web view not instantiated")
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
